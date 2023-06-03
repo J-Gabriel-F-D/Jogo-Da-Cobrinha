d41ce3b00ff42edce2d7ebcff2a544137c90f639
Jose Gabriel <jgabrielfd0.@gmail.com>
2023-06-03 11:03:23 -0300

    Criando a tela e o launcher do jogo

---
+++ Tela.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Tela extends JFrame{
+
+    public Tela(){
+        setTitle("tela");
+        setSize(512, 512);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+}
